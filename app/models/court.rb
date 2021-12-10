0788f1e8e41189d9ac920002a83b45bebb785557
takahashi-1234 <riki.1130@icloud.com>
2021-12-10 07:12:05 +0000

    [update]コメント機能

--- app/models/court.rb
+++ app/models/court.rb
@@ -3,6 +3,7 @@ class Court < ApplicationRecord
   has_many:comments,dependent: :destroy
   has_many:favorites,dependent: :destroy
   has_many:reports,dependent: :destroy
+  has_many:circles,dependent: :destroy
   
   def favorited_by?(customer)
     favorites.where(customer_id:customer.id).exists?
